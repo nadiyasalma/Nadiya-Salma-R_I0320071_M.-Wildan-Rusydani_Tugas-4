cf2acd40485da35a8560b5edaf3e99cd8599a8c8
nadiyasalma <nadiyasalma084@gmail.com>
2021-03-25 21:31:12 +0700

    Tugas 4 Praktikum Prokom

--- I0320071_soal4_tugas4.py
+++ I0320071_soal4_tugas4.py
@@ -1,11 +1,13 @@
 #program rencana pengujian
-usia = 21
-x = int(input("berapa usia anda : "))
-if usia < x
+usia_min= 21
+usia_pengguna = int(input("berapa usia anda : "))
+if usia_min <= usia_pengguna :
     print("anda sudah cukup umur")
-    y = input("apakah anda sudah lulus dalam ujian kulaifikasi: (Y/T) ")
-    if Y == 'T' :
+    a = str(input("apakah anda sudah lulus dalam ujian kulaifikasi: (Y/T) "))
+    if a == "T" :
         print("anda tidak dapat mendaftar di kursus ")
+    elif a == "T" :
+        print("anda belom boleh mengikuti kursus")
 else :
     print("anda belum cukup umur")
 
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Nadiya-Salma-R_I0320071_M.-Wildan-Rusydani_Tugas-4
+
+# daftar isi
+## exercise 4.1 - 4.12
+## soal nomer 2
+## soal nomer 3
+## soal nomer 4
+## soal nomer 5
+## soal nomer 6
